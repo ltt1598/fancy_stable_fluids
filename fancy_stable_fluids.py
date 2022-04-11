db84bdcb4d054e07a5e1f54abe699a3bf782d7ed
Tiantian Liu <ltt1598@gmail.com>
2022-04-11 18:40:33 +0800

    changing mouse events and dye splats

--- fancy_stable_fluids.py
+++ fancy_stable_fluids.py
@@ -11,28 +11,35 @@ import random
 ti.init(arch=ti.cpu)
 
 # constants
+# resolution control
 SCREEN_RES_X = 1920
 SCREEN_RES_Y = 1080
-texel_size_X = 1.0 / SCREEN_RES_X
-texel_size_y = 1.0 / SCREEN_RES_Y
-
+SCREEN_RES_X_RECIPROCAL = 1.0 / SCREEN_RES_X
+SCREEN_RES_Y_RECIPROCAL = 1.0 / SCREEN_RES_Y
 aspect_ratio = float(SCREEN_RES_X) / float(SCREEN_RES_Y)
-GRID_RES_Y = 64
-GRID_RES_X = int(GRID_RES_Y * aspect_ratio)
-
-grid_2_screen = float(SCREEN_RES_Y) / float(GRID_RES_Y)
-screen_2_grid = 1.0 / grid_2_screen
-
-dt = 0.0167
-p_jacobi_iters = 20  
-f_strength = 1e5
-curl_strength = 7.0
-time_c = 2
+SIM_RES_Y = 64 
+SIM_RES_X = int(SIM_RES_Y * aspect_ratio) 
+SIM_RES_X_RECIPROCAL = 1.0 / SIM_RES_X
+SIM_RES_Y_RECIPROCAL = 1.0 / SIM_RES_Y
+# assuming grid size dx = dy = 1
+sim_2_screen = float(SCREEN_RES_Y) / float(SIM_RES_Y)
+screen_2_sim = 1.0 / sim_2_screen
+# color
+dye_density = 0.15 # between 0 and 1
+# simulation parameters
 maxfps = 60
-dye_decay = 1 - 1 / (maxfps * time_c)
-# dye_decay = 1
-force_radius = SCREEN_RES_Y / 8.0
+dt = 1.0 / maxfps
+f_strength = 3e5
+curl_strength = 0
+dye_dissipation = 1
+vel_dissipation = 0.1
+dye_decay = 1.0 / (1 + dt * dye_dissipation)
+vel_decay = 1.0 / (1 + dt * vel_dissipation)
+force_radius = 2.5e-3
 paused = False
+# numerical solver parameters
+p_jacobi_iters = 20  
+p_jacobi_warm_starting = 0.8
 
 # taichi fields
 class TexPair:
@@ -43,19 +50,20 @@ class TexPair:
     def swap(self):
         self.cur, self.nxt = self.nxt, self.cur
 # simulation quantities
-_velocities = ti.Vector.field(2, float, shape=(GRID_RES_X, GRID_RES_Y))
-_new_velocities = ti.Vector.field(2, float, shape=(GRID_RES_X, GRID_RES_Y))
-_pressures = ti.field(float, shape=(GRID_RES_X, GRID_RES_Y))
-_new_pressures = ti.field(float, shape=(GRID_RES_X, GRID_RES_Y))
+_velocities = ti.Vector.field(2, float, shape=(SIM_RES_X, SIM_RES_Y))
+_new_velocities = ti.Vector.field(2, float, shape=(SIM_RES_X, SIM_RES_Y))
+_pressures = ti.field(float, shape=(SIM_RES_X, SIM_RES_Y))
+_new_pressures = ti.field(float, shape=(SIM_RES_X, SIM_RES_Y))
 velocities_pair = TexPair(_velocities, _new_velocities)
 pressures_pair = TexPair(_pressures, _new_pressures)
-velocity_divs = ti.field(float, shape=(GRID_RES_X, GRID_RES_Y))
-velocity_curls = ti.field(float, shape=(GRID_RES_X, GRID_RES_Y))
+velocity_divs = ti.field(float, shape=(SIM_RES_X, SIM_RES_Y))
+velocity_curls = ti.field(float, shape=(SIM_RES_X, SIM_RES_Y))
 
 # visualization quantities
 _dye_buffer = ti.Vector.field(3, float, shape=(SCREEN_RES_X, SCREEN_RES_Y))
 _new_dye_buffer = ti.Vector.field(3, float, shape=(SCREEN_RES_X, SCREEN_RES_Y))
 dyes_pair = TexPair(_dye_buffer, _new_dye_buffer)
+pixels = ti.Vector.field(3, float, shape=(SCREEN_RES_X, SCREEN_RES_Y))
 
 
 # simulation helpers
@@ -67,9 +75,9 @@ def lerp(vl, vr, frac):
 @ti.func
 def sample_sim(qf, u, v):
     i = int(u)
-    i = max(0, min(GRID_RES_X - 1, i))
+    i = max(0, min(SIM_RES_X - 1, i))
     j = int(v)
-    j = max(0, min(GRID_RES_Y - 1, j))
+    j = max(0, min(SIM_RES_Y - 1, j))
     return qf[i, j]
 
 @ti.func
@@ -125,11 +133,11 @@ def divergence(vf: ti.template()):
         vc = sample_sim(vf, i, j)
         if i == 0:
             vl.x = -vc.x
-        if i == GRID_RES_X - 1:
+        if i == SIM_RES_X - 1:
             vr.x = -vc.x
         if j == 0:
             vb.y = -vc.y
-        if j == GRID_RES_Y - 1:
+        if j == SIM_RES_Y - 1:
             vt.y = -vc.y
         velocity_divs[i, j] = (vr.x - vl.x + vt.y - vb.y) * 0.5
 
@@ -148,42 +156,38 @@ def vorticity(vf: ti.template()):
 def apply_impulse(vf: ti.template(), dyef: ti.template(),
                   imp_data: ti.ext_arr()):
 
-    omx, omy = imp_data[2], imp_data[3]
-    mdir = ti.Vector([imp_data[0], imp_data[1]])
+    omx, omy = imp_data[3], imp_data[4]
+    delta_mxy = ti.Vector([imp_data[1], imp_data[2]])
+    delta_mxy[0] *= aspect_ratio
 
     for i, j in vf:
-        dx, dy = ((i + 0.5) * grid_2_screen - omx), ((j + 0.5) * grid_2_screen - omy)
+        dx, dy = ((i + 0.5) * SIM_RES_X_RECIPROCAL - omx)* aspect_ratio, (j + 0.5) * SIM_RES_Y_RECIPROCAL - omy
         d2 = dx * dx + dy * dy
         # dv = F * dt
         factor = ti.exp(-d2 / force_radius)
-        momentum = (mdir * f_strength * factor) * dt * grid_2_screen
-        v = vf[i, j]
-        vf[i, j] = v + momentum
+        momentum = delta_mxy * f_strength * factor * dt
+        vf[i, j] += momentum 
 
     for i, j in dyef:
-        dx, dy = ((i + 0.5) - omx), ((j + 0.5) - omy)
+        dx, dy = ((i + 0.5) * SCREEN_RES_X_RECIPROCAL - omx) * aspect_ratio, (j + 0.5) * SCREEN_RES_Y_RECIPROCAL - omy 
         d2 = dx * dx + dy * dy        
-        dc = dyef[i, j]
-        # add dye
-        if mdir.norm() > 0.5:
-            dc += ti.exp(-d2 * (2 / (SCREEN_RES_Y / 16)**2)) * ti.Vector(
-                [imp_data[4], imp_data[5], imp_data[6]])
-        dyef[i, j] = dc
+        factor = ti.exp(-d2 / force_radius)
+        dyef[i, j] += factor * ti.Vector([imp_data[5], imp_data[6], imp_data[7]])
 
 @ti.kernel
 def advect_sim(vf: ti.template(), qf: ti.template(), new_qf: ti.template()):
     for i, j in qf:
         p = ti.Vector([i, j]) + 0.5
         p = backtrace(vf, p, dt)
-        new_qf[i, j] = bilerp_sim(qf, p)
+        new_qf[i, j] = bilerp_sim(qf, p) * vel_decay
 
 @ti.kernel
 def advect_dye(vf: ti.template(), qf: ti.template(), new_qf: ti.template()):
     for i, j in qf:
         p = ti.Vector([i, j]) + 0.5
-        p *= screen_2_grid
+        p *= screen_2_sim
         p = backtrace(vf, p, dt)
-        p *= grid_2_screen
+        p *= sim_2_screen
         new_qf[i, j] = bilerp_screen(qf, p) * dye_decay
 
 @ti.kernel
@@ -222,11 +226,18 @@ def enhance_vorticity(vf: ti.template(), cf: ti.template()):
         force = ti.Vector([abs(ct) - abs(cb),
                            abs(cl) - abs(cr)]).normalized(1e-3)
         force *= curl_strength * cc
-        vf[i, j] = min(max(vf[i, j] + force * dt, -1e3), 1e3)
+        vf[i, j] += force * dt
+        # vf[i, j] = min(max(vf[i, j] + force * dt, -1e3), 1e3)
+
+# render utils
+@ti.kernel
+def shading(dyef: ti.template(), pxf: ti.template()):
+    for i,j in pxf:
+        pxf[i, j] = dyef[i, j]
+
 
 # simulation main loop
 def step(mouse_data):
-
     # advect quantities
     advect_sim(velocities_pair.cur, velocities_pair.cur, velocities_pair.nxt)
     advect_dye(velocities_pair.cur, dyes_pair.cur, dyes_pair.nxt)
@@ -234,14 +245,15 @@ def step(mouse_data):
     dyes_pair.swap()
 
     # apply force
-    apply_impulse(velocities_pair.cur, dyes_pair.cur, mouse_data)
+    if mouse_data[0]: # mouse pressed and moved
+        apply_impulse(velocities_pair.cur, dyes_pair.cur, mouse_data)
     if curl_strength:
         vorticity(velocities_pair.cur)
         enhance_vorticity(velocities_pair.cur, velocity_curls)
 
     # pressure solve (jacobi)
     divergence(velocities_pair.cur)
-    decay_pressure(pressures_pair.cur, 0.8)
+    decay_pressure(pressures_pair.cur, p_jacobi_warm_starting)
     for _ in range(p_jacobi_iters):
         pressure_jacobi(pressures_pair.cur, pressures_pair.nxt)
         pressures_pair.swap()
@@ -249,10 +261,19 @@ def step(mouse_data):
     # update velocity
     subtract_gradient(velocities_pair.cur, pressures_pair.cur)
 
+def render():
+    shading(dyes_pair.cur, pixels)
+
 def reset():
     velocities_pair.cur.fill(0)
     pressures_pair.cur.fill(0)
     dyes_pair.cur.fill(0)
+
+# random color gen
+def generate_color():
+    c = np.array(colorsys.hsv_to_rgb(random.uniform(0, 1), 1.0, 1.0))
+    c *= dye_density
+    return c
 
 # mouse events
 class MouseDataGen(object):
@@ -262,39 +283,40 @@ class MouseDataGen(object):
         self.mouse_ticks = 0
 
     def __call__(self, window):
-        # [0:2]: normalized delta direction
-        # [2:4]: current mouse xy
-        # [4:7]: color
+        # [0]: whether mouse is moved
+        # [1:3]: normalized delta direction
+        # [3:5]: current mouse xy
+        # [5:8]: color
         mouse_data = np.zeros(8, dtype=np.float32)
         if window.is_pressed(ti.ui.LMB):
             mxy = np.array(window.get_cursor_pos(), dtype=np.float32)
-            mxy[0] *= SCREEN_RES_X
-            mxy[1] *= SCREEN_RES_Y
 
             # change dye color every 5 mouse events
-            if self.mouse_ticks < 5:
-                self.mouse_ticks += 1
-            else:
+            if self.mouse_ticks > 5:
                 self.mouse_ticks = 0
-                self.prev_color = colorsys.hsv_to_rgb(random.uniform(0, 1), 0.8, 1.0)
+                self.prev_color = generate_color()
 
-            if self.prev_mouse is None:
-                self.prev_mouse = mxy
+            if self.prev_mouse is None: # mouse pressed
                 self.mouse_ticks = 0
-                self.prev_color = colorsys.hsv_to_rgb(random.uniform(0, 1), 0.8, 1.0)
-            else:
-                mdir = mxy - self.prev_mouse
-                mdir = mdir / (np.linalg.norm(mdir) + 1e-5)
-                mouse_data[0], mouse_data[1] = mdir[0], mdir[1]
-                mouse_data[2], mouse_data[3] = mxy[0], mxy[1]
-                mouse_data[4:7] = self.prev_color
                 self.prev_mouse = mxy
+                self.prev_color = generate_color()
+            else: # mouse moving
+                self.mouse_ticks += 1
+                delta_mxy = mxy - self.prev_mouse
+                if np.linalg.norm(delta_mxy) > 1e-4:
+                    mouse_data[0] = 1
+                    mouse_data[1], mouse_data[2] = delta_mxy[0], delta_mxy[1]
+                    mouse_data[3], mouse_data[4] = mxy[0], mxy[1]
+                    mouse_data[5:8] = self.prev_color
+                self.prev_mouse = mxy
+                
         else:
+            mouse_data[0] = 0
             self.prev_mouse = None
             self.prev_color = None
         return mouse_data
 
-window = ti.ui.Window('Stable Fluid', (SCREEN_RES_X, SCREEN_RES_Y), vsync=True)
+window = ti.ui.Window('Fancy Stable Fluids', (SCREEN_RES_X, SCREEN_RES_Y), vsync=True)
 canvas = window.get_canvas()
 md_gen = MouseDataGen()
 
@@ -306,17 +328,13 @@ while window.running:
         elif e.key == 'r':
             paused = False
             reset()
-        elif e.key == 's':
-            if curl_strength:
-                curl_strength = 0
-            else:
-                curl_strength = 7
         elif e.key == 'p':
             paused = not paused
 
     if not paused:
         mouse_data = md_gen(window)
         step(mouse_data)
+        render()
 
-    canvas.set_image(dyes_pair.cur)
+    canvas.set_image(pixels)
     window.show()
